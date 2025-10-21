5e43effc79cc658b954312b1c0520b2c8fad5373
Cintia Losada <cinlos3382@gmail.com>
2025-10-21 20:16:20 -0300

    Agregar scripts de Python para ilustrar operadores aritméticos y de asignación

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,17 @@
+#operadores aritméticos 
+numero1 = 10
+numero2 = 5 #operaador asignación =
+
+suma = numero1 + numero2  #operador suma +
+resta = numero1 - numero2  #operador resta -
+multiplicacion = numero1 * numero2  #operador multiplicación *
+division = numero1 / numero2  #operador división /
+modulo = numero1 % numero2  #operador módulo %
+
+print("************* CALCULADORA *****************")
+
+print(f"La suma es: {suma}")
+print(f"La resta es: {resta}")
+print(f"La multiplicación es: {multiplicacion}")
+print(f"La división es: {division}")
+print(f"El módulo es: {modulo}")
---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,25 @@
+#o´peradores de asignacion
+
+edad = 30
+print(edad)
+
+edad += 5  # edad = edad + 5
+print(edad)
+
+#operadores de incremento y decremento
+
+year = 2024
+
+"""
+year++
+year--
+++year
+--year
+No existen en Python
+"""
+
+year += 1  # year = year + 1
+print(year)
+year -= 1  # year = year - 1
+print(year)
+
