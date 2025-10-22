f7c57c98ab0a6826f6da27272e91c8c6f0d4de27
Cintia Losada <cinlos3382@gmail.com>
2025-10-22 18:26:37 -0300

    Agregar script de Python para entrada y salida de datos

---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,6 @@
+#entrada
+nombre = input("¿Cuál es tu nombre?: ")
+edad = input("¿Cuántos años tienes?: ")
+
+#salida
+print(f"Hola {nombre}, bienvenido al curso de Python!, veo que tienes {edad} años.")
